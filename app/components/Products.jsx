cf9796ae77efe3b4538c1f28ebe4fdb39efd3aa1
Stanlisberg <nnabugo2010@gmail.com>
2024-01-24 05:54:58 +0100

    Feature: Establish api data

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function Products() {
-  return (
-    <div>Products</div>
+const getProducts = async () => {
+  const res = await fetch("http://localhost:3003/product");
+  return res.json();
+};
+
+async function Products() {
+  const products = await getProducts();
+  console.log(products);
+  return(
+    <div>
+      {products.map((product, index) => (
+        <div key={index}>
+          {product.id}
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Products
+export default Products;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import Products from "./components/Products";
 
 export default function Page() {
   return (
     <div>
      <Home />
+     <Products />
     </div>
   );
 }
